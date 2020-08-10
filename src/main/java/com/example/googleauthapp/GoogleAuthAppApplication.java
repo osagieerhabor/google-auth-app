af7076737bd5035805b16fe2305f6a346e5ecaf9
osagieerhabor <osagie.erhabor@exquisappfactory.com>
2020-08-10 13:02:02 +0100

    First commot

---
+++ src/main/java/com/example/googleauthapp/GoogleAuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.googleauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAuthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAuthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/googleauthapp/GoogleAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googleauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
